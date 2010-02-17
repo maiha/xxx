fcd5cf9d155a5e46f4a6e73247c4927659e819e5
maiha <maiha@wota.jp>
2010-02-17 16:11:38 +0900

    ready

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+
+require 'spec'
+require 'rr'
+
+Dir.glob(File.join(File.dirname(__FILE__), '/../lib/*.rb')).each{|lib| require lib}
+require File.join(File.dirname(__FILE__), '/its_helper')
+require File.join(File.dirname(__FILE__), '/provide_helper')
+
+
+def path(key)
+  Pathname(File.join(File.dirname(__FILE__) + "/fixtures/#{key}"))
+end
+
+def data(key)
+  (@__fixture_data_cache__ ||= {})[key] ||= path(key).read{}
+end
