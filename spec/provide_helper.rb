7101040eec60e0d8b64514a9237985f581ecf878
maiha <maiha@wota.jp>
2010-03-31 05:48:23 +0900

    Update document and refactor provide method

--- spec/provide_helper.rb
+++ spec/provide_helper.rb
@@ -1,8 +1,9 @@
 ######################################################################
 ### provide matcher
+
 Spec::Matchers.define :provide do |expected|
   match do |obj|
-    (obj.public_methods + obj.protected_methods + obj.private_methods).include?(expected.to_s)
+    !!(obj.method(expected) rescue false)
   end
 end
 
@@ -12,17 +13,37 @@ module Spec
       module ExampleGroupMethods
         # == Examples
         #
-        #   describe User do
-        #     subject { User.new }
-        #     provide :name
+        #   class Convert
+        #     private
+        #       def execute(value)
         #
-        # [intead of]
+        # 'provide' method checks whether subject has it
         #
-        #     it "should provide #name" do
-        #       methods = subject.public_methods + subject.protected_methods + subject.private_methods
-        #       methods.should include("name")
-        #     end
-        #   end
+        #   describe Convert do
+        #     provide :execute   # 1)
+        #
+        #   [instead of]
+        #
+        #   describe Convert do
+        #     it "should provide #execute" do
+        #       subject.should provide(:execute)
+        #
+        #
+        # Further more, it provides two features when block given
+        #   1) defines a new context for it
+        #   2) creates helper method for it (useful for private method)
+        #
+        #   describe Convert do
+        #     provide :execute do
+        #       it "should return nil when nil is given" do
+        #         execute(nil).should == nil
+        #
+        #   [instead of]
+        #
+        #   describe Convert do
+        #     describe "#execute" do                               # 1)
+        #       it "should return nil when nil is given" do
+        #         subject.__send__(:execute, nil).should == nil    # 2)
         #
         def provide(name, &block)
           it "should provide ##{name}" do
@@ -32,7 +53,7 @@ module Spec
           if block
             describe("##{name}") do
               define_method(name) do |*args|
-                subject.send(name, *args)
+                subject.__send__(name, *args)
               end
               class_eval(&block)
             end
